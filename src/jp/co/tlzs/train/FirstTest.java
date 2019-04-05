b06660eb7db3414c80852139fefa7057d4495a87
YaoHua1105 <kakeisyou1105@yahoo.co.jp>
2019-04-05 16:21:58 +0900

    again

--- src/jp/co/tlzs/train/FirstTest.java
+++ src/jp/co/tlzs/train/FirstTest.java
@@ -3,7 +3,7 @@ package jp.co.tlzs.train;
 public class FirstTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Kitty");
+		System.out.println("Hello Kitty again");
 
 
 	}
